c3837a91d997916919753f9c8b76a460c0859a01
Selsabeel A <90451756+SelsabeelA@users.noreply.github.com>
2022-12-08 10:49:48 +0200

    Update A3_SheetPb10_20210714.cpp

--- A3_SheetPb10_20210714.cpp
+++ A3_SheetPb10_20210714.cpp
@@ -6,63 +6,104 @@ Author: Selsabeel Asim Ali Elbagir
  Assignment: 3
  Course: CS213
  Date created: 7th December, 2022
-Purpose:
+Purpose: A program that converts the date from month/day format to alphabetic month/day.
+ Contains two exception classes, MonthError and DayError.
  */
-/*Exceptions. Ihab Ashraf is trying to write a program that converts dates from numerical month/day
-format to alphabetic month/day (for example, 1/31 or 01/31 corresponds to January 31). You will define
-two exception classes, one called MonthError and another called DayError. If the user enters
-anything other than a legal month number (integers from 1 to 12), then your program will throw and
-catch a MonthError. Similarly, if the user enters anything other than a valid day number (integers
-from 1 to either 29, 30, or 31, depending on the month), then your program will throw and catch a
-DayError. To keep things simple, always allow 29 days for February.*/
 
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
 string FixDate(int month,int day){
-    string word = "";
-    switch(month){
-        case(1):
-            word+="January";
-        case(2):
-            word+="February";
-        case(3):
-            word+="March";
-        case(4):
-            word+="April";
-        case(5):
-            word+="May";
-        case(6):
-            word+="June";
-        case(7):
-            word+="July";
-        case(8):
-            word+="August";
-        case(9):
-            word+="September";
-        case(10):
-            word+="October";
-        case(11):
-            word+="November";
-        case(12):
-            word+="December";
-        default:
-            throw ;
+    try{
+        string word = "";
+        switch(month){
+            case(1):{
+                word+="January";
+                break;
+            }
+            case(2):{
+                word+="February";
+                break;
+            }
+            case(3):{
+                word+="March";
+                break;
+            }
+            case(4):{
+                word+="April";
+                break;
+            }
+            case(5):{
+                word+="May";
+                break;
+            }
+            case(6):{
+                word+="June";
+                break;
+            }
+            case(7):{
+                word+="July";
+                break;
+            }
+            case(8):{
+                word+="August";
+                break;
+            }
+            case(9):{
+                word+="September";
+                break;
+            }
+            case(10):{
+                word+="October";
+                break;
+            }
+            case(11):{
+                word+="November";
+                break;
+            }
+            case(12):{
+                word+="December";
+                break;
+            }
+
+        }
+        if(month<1 || month>12){
+            throw "MonthError";
+        }
+        if(day>31 || day<1){
+            throw "DayError";
+        }
+        else if(day>30 && (month==4 || month==6 || month==9 || month==11)){
+            throw "DayError";
+        }
+        else if(day>29 && month==2){
+            throw "DayError";
+        }
+        string result = word + " " + to_string(day);
+        cout << result << endl;
+        return result;
     }
-    if(day>31 || day<1){
-        throw "DayError";
+    catch(string error){
+        if(error == "MonthError"){
+            cout << "ERROR! You have entered an invalid month.";
+        }
+        if(error == "DayError"){
+            cout << "ERROR! You have entered an invalid day.";
+        }
     }
-    else if(day>30 && (month==4 || month==6 || month==9 || month==11)){
-        throw "DayError";
-    }
-    else if(day>29 && month==2){
-        throw "DayError";
-    }
-    string result = word + " " + to_string(day);
-    cout << result;
-    return result;
 }
+
 int main(){
+    while(true){
+        // this code will run until it has to throw an exception
+        string word;
+        cout << "\nPlease enter the date in month/day format!:";
+        cin >> word;
+        int slash_position = word.find('/');
+        int month = stoi(word.substr(0,slash_position) );
+        int day = stoi(word.substr(slash_position+1,word.size()) );
+        FixDate(month, day);
+    }
 
 }
