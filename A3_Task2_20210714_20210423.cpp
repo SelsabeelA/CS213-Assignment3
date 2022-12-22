4cd83a892f3fb3683f3fe32ee27106028eeb42a9
Selsabeel A <90451756+SelsabeelA@users.noreply.github.com>
2022-12-22 17:10:16 +0200

    Update A3_Task2_20210714_20210423.cpp

--- A3_Task2_20210714_20210423.cpp
+++ A3_Task2_20210714_20210423.cpp
@@ -10,19 +10,31 @@ int main(){
     vec.push_back((string) "Guten Tag");
     vec.push_back((string) "Salam Alykum");
     vec.push_back((string) "Hola");
-    cout << vec.Size() << endl;
+    cout << vec << endl;
+    cout << "String vector's size: " << vec.Size() << endl;
+    vec.erase(vec.begin());
+    cout << "size after erasing first element" << vec.Size() << endl;
+    cout << vec;
 
     SNVector<int> intvec(4);
-    intvec.push_back(4);
-    intvec.push_back(3);
-    intvec.push_back(2);
-    intvec.push_back(5);
-    intvec.begin()++;
-    cout << intvec.Size();
+    intvec.push_back(40);
+    intvec.push_back(32);
+    intvec.push_back(22);
+    intvec.push_back(53);
+    cout << intvec << endl;
+    cout << "Integer vector's size: " << intvec.Size() << endl;
     intvec.erase(intvec.begin());
-    cout << intvec.Size();
-    cout << endl;
+    cout << "size after erasing first element" << intvec.Size() << endl;
     cout << intvec;
 
+    double dbl_array[4] = {22.22,23.22,22.3,0.2};
+    SNVector<double> dbl_vec(dbl_array, 4);
+    cout << "Double vector's size: " << dbl_vec.Size() << endl;
+    dbl_vec.clear();
+    cout << "Double vector's size: " << dbl_vec.Size() << endl;
+    dbl_vec.resize();
+    dbl_vec.push_back(33.3);
+    cout << "Double vector's size: " << dbl_vec.Size() << endl;
+    dbl_vec.erase(dbl_vec.begin());
     return 0;
 }
